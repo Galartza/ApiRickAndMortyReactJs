a4dc873fbb2aac6d7fae00132fb648ea0a203204
Galartza <jotaquin99g@gmail.com>
2024-02-22 16:50:59 -0300

    Agregando estilos responsivos y cards1.1

--- src/components/Home/home-styles.js
+++ src/components/Home/home-styles.js
@@ -129,6 +129,41 @@ export const HomeSection = styled.section`
             & .shape {
             fill: #0E1822;
             }
+
+            .modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 10% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
 `
 export const HomeContainerUno = styled.div`
            height: 30rem;
